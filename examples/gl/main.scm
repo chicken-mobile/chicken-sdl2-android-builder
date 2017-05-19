efc21a4a82618b88074b4dab39d2d30c79ed49f2
Kristian Lein-Mathisen <kristianlein@gmail.com>
2017-05-19 09:18:47 +0200

    p/fade-out working

--- examples/gl/main.scm
+++ examples/gl/main.scm
@@ -6,143 +6,234 @@
      miscmacros)
 
 (import (prefix sdl2 sdl2:)
-        (prefix opengl-glew gl:))
+        (prefix opengl-glew gl:)
+        (prefix gl-utils gl-utils:)
+        (prefix gl-math gl-math:))
 
-(sdl2:set-main-ready!)
-(sdl2:init! '(video timer))
-(on-exit sdl2:quit!)
-
-(define window (sdl2:create-window!
-                "SDL2 + OpenGL Example"
-                'undefined 'undefined 640 480
-                '(opengl)))
-
-;;; Use OpenGL 3.3
-(sdl2:gl-attribute-set! 'context-profile-mask 'es)
-(sdl2:gl-attribute-set! 'context-major-version 3)
-(sdl2:gl-attribute-set! 'context-minor-version 0)
-
-(define gl-context (sdl2:gl-create-context! window))
-
-(assert (equal? window     (sdl2:gl-get-current-window)))
-(assert (equal? gl-context (sdl2:gl-get-current-context)))
-
-(gl:init)
-(gl-utils:check-error)
+(include "sdl2-setup.scm")
 
 
-(begin ;; eval this (M-C-x in emacs) to hot-swap the program into the running app
+;; ==================== utils ====================
 
-  (define vertex-shader-source "
+(define-syntax with-texture
+  (syntax-rules ()
+    ((_ id body ...)
+     (dynamic-wind (lambda () (gl:bind-texture gl:+texture-2d+ id))
+                   (lambda () body ...)
+                   (lambda () (gl:bind-texture gl:+texture-2d+ 0))))))
+
+;; ==================== program record ====================
+(define-record program id)
+(define (create-program vertex-shader fragment-shader)
+  (let* ((vertex-shader (or vertex-shader "
 #version 300 es
 in vec2 position;
-in vec3 color;
-out vec3 c;
-uniform mat4 MVP;
-
 void main(){
-   gl_Position = MVP * vec4(position, 0.0, 1.0);
-   c = color;
-}")
+   gl_Position = vec4(position, 0.0, 1.0);
+}"))
+         (id (gl-utils:make-program
+              (list (if (string? vertex-shader)
+                        (gl-utils:make-shader gl:+vertex-shader+ vertex-shader)
+                        vertex-shader)
+                    (if (string? fragment-shader)
+                        (gl-utils:make-shader gl:+fragment-shader+ fragment-shader)
+                        fragment-shader)))))
 
-  (define fragment-shader-source "
+    (set-finalizer! (make-program id)
+                    (lambda (p)
+                      (print "deleting program " p)
+                      (gl:delete-program (program-id p))))))
+
+(define-syntax with-program
+  (syntax-rules ()
+    ((_ p body ...)
+     (dynamic-wind (lambda () (gl:use-program (if (program? p) (program-id p) p)))
+                   (lambda () body ...)
+                   (lambda () (gl:use-program 0))))))
+
+
+;; a canvas is basically a framebuffer with texture. you can render to
+;; a canvas using its framebuffer, and you can read its output from
+;; its texture.
+(define-record canvas fb tex w h)
+
+(define (create-canvas w h #!key (format gl:+rgb+) (type gl:+unsigned-byte+))
+  (let ((tex (gl-utils:gen-texture))
+        (fbo (gl-utils:gen-framebuffer)))
+
+    (gl-utils:set-texture-properties tex)
+
+    (with-texture
+     tex
+     (gl:tex-image-2d gl:+texture-2d+ 0 format w h 0 format type #f))
+
+    (gl:bind-framebuffer gl:+framebuffer+ fbo)
+    (gl:framebuffer-texture-2d gl:+framebuffer+ gl:+color-attachment0+
+                               gl:+texture-2d+ tex 0)
+
+    (set-finalizer! (make-canvas fbo tex w h)
+                    (lambda (c)
+                      (print "gc: canvas cleanup " c)
+                      (gl-utils:delete-texture (canvas-tex c))
+                      (gl-utils:delete-framebuffer (canvas-fb c))))))
+
+(define (canvas-swap! a b)
+  (let ((t (canvas-tex b))) (canvas-tex-set! b (canvas-tex a)) (canvas-tex-set! a t))
+  (let ((t (canvas-fb  b))) (canvas-fb-set!  b (canvas-fb  a)) (canvas-fb-set!  a t))
+  (let ((t (canvas-w   b))) (canvas-w-set!   b (canvas-w   a)) (canvas-w-set!   a t))
+  (let ((t (canvas-h   b))) (canvas-h-set!   b (canvas-h   a)) (canvas-h-set!   a t)))
+
+(define-syntax with-output-to-canvas
+  (syntax-rules ()
+    ((_ id body ...)
+     (dynamic-wind (lambda () (gl:bind-framebuffer gl:+framebuffer+
+                                              (if (canvas? id) (canvas-fb id) id)))
+                   (lambda () body ...)
+                   (lambda () (gl:bind-framebuffer gl:+framebuffer+ 0))))))
+
+
+
+
+;; we really just want to incoke fragment shaders for all pixels. this
+;; does something like that.
+(define render-square
+  (let ((square (gl-utils:make-mesh
+                 vertices: '(attributes:
+                             ((position #:float 2))
+                             initial-elements:
+                             ((position . (-1 -1
+                                              1 -1
+                                              1  1
+                                              -1  1))))
+                 indices: '(type: #:ushort
+                                  initial-elements: (0 1 2
+                                                       0 2 3)))))
+    ;; normally, you'd do this: (gl:get-attrib-location (program-id program) "position")
+    ;; 
+    ;; but we have lots of different programs around. all of them use
+    ;; the same vertex-shader, though, which even has only one
+    ;; attribute. so we should be able to hardcode 0.
+    (gl-utils:mesh-make-vao! square `((position . 0)))
+    (lambda ()
+      (gl:bind-vertex-array (gl-utils:mesh-vao square))
+      (gl:draw-elements
+       (gl-utils:mode->gl (gl-utils:mesh-mode square))
+       (gl-utils:mesh-n-indices square)
+       (gl-utils:type->gl (gl-utils:mesh-index-type square))
+       #f))))
+
+
+(define p/fade-out
+
+  (let ((program-blue
+         (create-program
+          #f
+          "
 #version 300 es
-in vec3 c;
-out vec4 fragColor;
-void main(){
-  fragColor = vec4(c, 1.0);
-}")
+precision mediump float;
 
-  (define program
-    (gl-utils:make-program
-     (list (gl-utils:make-shader gl:+vertex-shader+
-                                 vertex-shader-source)
-           (gl-utils:make-shader gl:+fragment-shader+
-                                 fragment-shader-source)))))
+out vec4 FragColor;
+
+uniform sampler2D VelocityTexture;
+uniform vec2 InverseSize;
+uniform float amount;
+
+void main() {
+ vec2 fragCoord = gl_FragCoord.xy;
+ FragColor = texture(VelocityTexture, InverseSize * fragCoord) + amount;
+}
+")))
+
+    (lambda (out source amount)
+      (with-output-to-canvas
+       out
+       (with-program
+        program-blue
+        (gl:uniform2f (gl:get-uniform-location (program-id program-blue) "InverseSize")
+                      (/ 1 (canvas-w out))
+                      (/ 1 (canvas-h out)))
+        (gl:uniform1f (gl:get-uniform-location (program-id program-blue) "amount")
+                      amount)
+
+        (gl:bind-texture gl:+texture-2d+ (canvas-tex source))
+        (render-square))))))
+
+(define (p/fill out r g b a)
+  (with-output-to-canvas
+   out
+   (gl:clear-color r g b a)
+   (gl:clear gl:+color-buffer-bit+)))
+
+(define (p/pset out   x y   r g b)
+  (with-texture
+   (canvas-tex out)
+   (gl:tex-sub-image-2d gl:+texture-2d+ ;; target
+                        0               ;; level
+                        x               ;; x
+                        y               ;; y
+                        1               ;; w
+                        1               ;; h
+                        gl:+rgb+
+                        gl:+unsigned-byte+
+                        (gl-utils:->pointer
+                         (string (integer->char (min 255 (inexact->exact (floor (* 255.0 r)))))
+                                 (integer->char (min 255 (inexact->exact (floor (* 255.0 g)))))
+                                 (integer->char (min 255 (inexact->exact (floor (* 255.0 b))))))))))
 
 
 
-(define square
-  (gl-utils:make-mesh
-   vertices: '(attributes:
-               ((position #:float 2)
-                (color #:unsigned-byte 3 normalized: #t))
-               initial-elements:
-               ((position . (-1 -1
-                              1 -1
-                              1  1
-                             -1  1))
-                (color . (255 0   0
-                          0   255 0
-                          0   0   255
-                          255 0   255))))
-   indices: '(type: #:ushort
-              initial-elements: (0 1 2
-                                 0 2 3))))
+(define c0 (create-canvas 512 512))
+(define c1 (create-canvas 512 512))
 
-(gl-utils:mesh-make-vao!
- square `((position . ,(gl:get-attrib-location program "position"))
-          (color    . ,(gl:get-attrib-location program "color"))))
+;; ==================== read pixels
+;; (define pixels (make-string (* 128 128 4) #\null))
+;; (gl-utils:with-framebuffer
+;;  frame_buffer
+;;  (gl:read-pixels 0 0 128 128
+;;                  gl:+rgb+
+;;                  gl:+unsigned-byte+
+;;                  (gl-utils:->pointer pixels)
+;;                  ))
 
-
-
-;;; A matrix representing the screen projection.
-(define projection-matrix
-  (gl-math:perspective 640 480 0.1 100 70))
-
-;;; A matrix representing the camera position and orientation.
-(define view-matrix
-  (gl-math:look-at (gl-math:make-point 1 0 3)
-                   (gl-math:make-point 0 0 0)
-                   (gl-math:make-point 0 1 0)))
-
-;;; The model's current rotation around the Z axis.
-(define *model-z-angle* 0.0)
-
-;;; Calculate a matrix representing the model's transformation.
-(define (calc-model-matrix)
-  (gl-math:z-rotation *model-z-angle*))
-
-;;; Calculate a matrix combining the model, view, and projection
-;;; matrices.
-(define (calc-mvp-matrix)
-  (gl-math:m* projection-matrix
-              (gl-math:m* view-matrix
-                          (calc-model-matrix))))
 
 (define (handle event)
   (case (sdl2:event-type event)
+    ((window)
+     (receive (w h) (sdl2:window-size window)
+       (gl:viewport 0 0 w h)))
     ((mouse-motion)
-     (print "handling mouse-motion event: " event))
+     ;;(print "handling mouse-motion event: " event)
+     (define mx (sdl2:mouse-motion-event-x event))
+     (define my (sdl2:mouse-motion-event-y event))
+
+     (receive (w h) (sdl2:window-size window)
+       (p/pset c0
+               (* (canvas-w c0) (/ mx w))
+               (* (canvas-h c0) (- 1 (/ my h)))
+               255 255 0)))
     (else (print "unhandled " event))))
 
 
-(define (render)
-  (gl:use-program program)
+(define (render canvas)
+  (p/fade-out (receive (w h) (sdl2:window-size window)
+                (make-canvas 0 #f w h))
+              canvas
+              0)
+  (gl-utils:check-error))
 
-  (gl:uniform-matrix4fv (gl:get-uniform-location program "MVP")
-                        1 #f
-                        (calc-mvp-matrix))
-
-  (gl:bind-vertex-array (gl-utils:mesh-vao square))
-  (gl:draw-elements
-   (gl-utils:mode->gl (gl-utils:mesh-mode square))
-   (gl-utils:mesh-n-indices square)
-   (gl-utils:type->gl (gl-utils:mesh-index-type square))
-   #f)
-
-  (gl-utils:check-error)
-  (gl:bind-vertex-array 0))
 
 (define core-iteration
   (let ((event (sdl2:make-event)))
     (lambda ()
       (while* (sdl2:poll-event! event) (handle it))
-      (gl:clear (bitwise-ior gl:+color-buffer-bit+
-                             gl:+depth-buffer-bit+))
 
-      (set! *model-z-angle* (* (sdl2:get-ticks) 0.001))
-      (render)
+      (gl:clear-color 0 0 0 0)
+      (gl:clear gl:+color-buffer-bit+)
+
+      (p/fade-out c1 c0 -0.005)
+      (canvas-swap! c0 c1)
+
+      (render c0)
       (sdl2:gl-swap-window! window))))
 
 
